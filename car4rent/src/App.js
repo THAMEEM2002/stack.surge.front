760dbc956623107ab7cccc139827f480885a9fb3
Nerdycobra <sivaprasadas01@gmail.com>
2024-07-02 14:29:03 +0530

    Merge branch 'main' of https://github.com/THAMEEM2002/stack.surge.front

--- car4rent/src/App.js
+++ car4rent/src/App.js
@@ -1,6 +1,11 @@
-import './App.css';
-import {BrowserRouter,Route, Link, Routes} from "react-router-dom"
-import Login from './components/Login'
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
+import './App.css'; // Import App.css
+import './components/Login.css'; // Import Login.css
+import './components/content.css'; // Import Content.css
+import Login from './components/Login';
+import Content from './components/content';
+
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
@@ -13,9 +18,30 @@ function App() {
   };
 
   return (
-    <div>
-      <Login/>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/content">Content</Link>
+            </li>
+          </ul>
+          {isLoggedIn ? (
+            <button onClick={handleLogout}>Logout</button>
+          ) : (
+            <Link to="/login">Login</Link>
+          )}
+        </nav>
+
+        <div className="container">
+          <Routes>
+            <Route path="/login" element={<Login onLogin={handleLogin} />} />
+            <Route path="/content" element={isLoggedIn ? <Content /> : <Navigate to="/login" />} />
+            <Route path="*" element={<Navigate to="/login" />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
