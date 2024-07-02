d61cc9f5528531058d629d5418d822bba9865d1c
MITS <ar6026454@gmail.com>
2024-07-02 11:27:58 +0530

    add background to app

--- car4rent/src/App.js
+++ car4rent/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import {BrowserRouter,Route, Link, Routes} from "react-router-dom"
 import Login from './components/Login'
 function App() {
   return (
     <div>
-      <Login/>
+      <main>
+        <BrowserRouter>
+        <Routes>
+      <Route path="/components/Login.js" element={<Login/>}/> 
+      <nav>
+      <ul>
+        <li>
+          <Link to = {"/components"}>Login</Link>
+        </li>
+      </ul>
+      </nav>
+    </Routes>
+    </BrowserRouter>
+    </main>
     </div>
   );
 }
